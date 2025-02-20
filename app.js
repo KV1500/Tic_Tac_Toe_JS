404fc11f6f8c1d8c515c81da23cbc9fee8136983
KV1500 <kunalvisave@gmail.com>
2025-02-20 18:35:19 +0530

    Game winning logic Js

--- app.js
+++ app.js
@@ -26,8 +26,22 @@ boxes.forEach((box)=>{
             turn0 = true;
         }
         box.ariaDisabled = true;
+
+        checkWinner();
     });
 });
 
+const checkWinner =()=>{
+    for(let pattern of winpattern){
+        let pos1val1 = boxes[pattern[0]].innerText;
+        let pos1val2 = boxes[pattern[1]].innerText;
+        let pos1val3 = boxes[pattern[2]].innerText;
 
+        if(pos1val1 !="" && pos1val2 !="" && pos1val3 !=""){
+            if(pos1val1=== pos1val2&& pos1val2===pos1val3){
+                console.log("Jinaklaa na bhaii");
+            }
+        }
+    }
+}
 
